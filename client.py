b796ee2d1013e5d5a10eb453c1669846afc9759c
Jiaxi Li <ljx@MacBook-15.local>
2023-07-26 14:48:49 -0500

    chore

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ from io import BytesIO
 cap = cv2.VideoCapture(1)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.0.128', 8080))
+client_socket.connect(('localhost', 8080))
 
 while cap.isOpened():
     _, frame = cap.read()
